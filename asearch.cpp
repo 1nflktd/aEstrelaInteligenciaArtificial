ddf709b8e10b89b5403eb68e635d47cd298213d3
Henrique <henscalon@hotmail.com>
2017-04-12 21:13:47 -0300

    refactoring

--- asearch.cpp
+++ asearch.cpp
@@ -12,6 +12,27 @@ namespace constants
     const int negative = -1;
 }
 
+namespace helpers
+{
+    template<typename T, typename V>
+    void remove(T & container, const V & v)
+    {
+        container.erase(std::remove(container.begin(), container.end(), v), container.end());
+    }
+
+    template<typename T, typename V>
+    bool find(const T & container, const V & v)
+    {
+        return std::find(container.begin(), container.end(), v) != container.end();
+    }
+
+    template<typename T, typename V>
+    int position(const T & container, const V & v)
+    {
+        return std::distance(container.begin(), std::find(container.begin(), container.end(), v));
+    }
+}
+
 struct ValDefaultInf 
 {
     int v = constants::infinity;
@@ -29,13 +50,28 @@ enum class Heuristic
 };
 
 template<typename T>
-std::ostream &operator <<(std::ostream &os, const std::vector<T> &v)
+using Vec = std::vector<T>;
+
+template<typename T>
+using Vec2d = std::vector<std::vector<T>>;
+
+template<typename T1, typename T2>
+using Map = std::map<T1, T2>;
+
+template<typename T>
+std::ostream &operator <<(std::ostream &os, const Vec<T> &v)
 {
-   std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
-   return os;
+   //std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+    int i = 1;
+    for (const auto & e : v) 
+    {
+        os << e << " ";
+        if (i++ % 3 == 0) os << "\n";
+    }
+    return os;
 }
 
-int heuristicOutOfPosition(const std::vector<int> & start, const std::vector<int> & goal) 
+int heuristicOutOfPosition(const Vec<int> & start, const Vec<int> & goal) 
 {
     int numbersOutPosition = 0;
     for (int i = 0; i < 9; ++i) 
@@ -46,7 +82,7 @@ int heuristicOutOfPosition(const std::vector<int> & start, const std::vector<int
     return numbersOutPosition;
 }
 
-int heuristicSumOfDiff(const std::vector<int> & start, const std::vector<int> & goal) 
+int heuristicSumOfDiff(const Vec<int> & start, const Vec<int> & goal) 
 {
     int sumDiff = 0;
     for (int i = 0; i < 9; ++i) 
@@ -57,7 +93,7 @@ int heuristicSumOfDiff(const std::vector<int> & start, const std::vector<int> & 
     return sumDiff;
 }
 
-int heuristic(const std::vector<int> & start, const std::vector<int> & goal, Heuristic h)
+int heuristic(const Vec<int> & start, const Vec<int> & goal, Heuristic h)
 {
     if (start.size() != goal.size() || start.size() != 9 || goal.size() != 9) 
     {
@@ -75,30 +111,24 @@ int heuristic(const std::vector<int> & start, const std::vector<int> & goal, Heu
     }
 }
 
-void printMap(const std::map<std::vector<int>, std::vector<int>> & cameFrom, std::vector<int> current) 
+void printMap(const Map<Vec<int>, Vec<int>> & cameFrom, Vec<int> current) 
 {
-    std::cout << current << "\n";
-
-    for(;;) 
+    for(int l = 1;; ++l) 
     {
+        std::cout << l << ":\n" << current;
         auto it = cameFrom.find(current);
-        if (it != cameFrom.end())  // enquanto existir posicao no map
-        {
-            current = it->second;
-            std::cout << current << "\n";
-        }
-        else 
-            break;
+        if (it == cameFrom.end()) break; // while there are positions it map
+        current = it->second;
     }
 }
 
-std::vector<int> lowestFScore(const std::vector<std::vector<int>> & openSet, const std::map<std::vector<int>, ValDefaultInf> & fScore) 
+Vec<int> lowestFScore(const Vec2d<int> & openSet, const Map<Vec<int>, ValDefaultInf> & fScore) 
 {
     ValDefaultInf lowestValue;
-    std::vector<int> lowestNode;
+    Vec<int> lowestNode;
+
     for (const auto & node : openSet) 
     {
-        // std::cout << "node " << node << "\n";
         auto it = fScore.find(node);
         if (it != fScore.end())
         {
@@ -109,20 +139,21 @@ std::vector<int> lowestFScore(const std::vector<std::vector<int>> & openSet, con
             }
         }
     }
+
     return lowestNode;
 }
 
-std::vector<std::vector<int>> calculateNeighboors(const std::vector<int> & node) 
+Vec2d<int> calculateNeighboors(const Vec<int> & node) 
 {
-    int positionZero = std::distance(node.begin(), std::find(node.begin(), node.end(), 0)); // posicao do zero
-    if (positionZero == static_cast<int>(node.size())) // se for igual a 9, deu erro
+    int positionZero = helpers::position(node, 0);
+    if (positionZero == static_cast<int>(node.size())) // if its equal to nodes size, its an error
         return {}; // error, throw exception
     
-    std::vector<std::vector<int>> posChanges = {
+    Vec2d<int> posChanges = {
         {1, 3}, {0, 2, 4}, {1, 5}, {0, 4, 6}, {1, 3, 5, 7}, {4, 2, 8}, {3, 7}, {6, 4, 8}, {7, 5}
     };
 
-    std::vector<std::vector<int>> neighboors;
+    Vec2d<int> neighboors;
     auto posVec = posChanges[positionZero];
     for (const auto & p : posVec)
     {
@@ -135,7 +166,7 @@ std::vector<std::vector<int>> calculateNeighboors(const std::vector<int> & node)
     return neighboors;
 }
 
-int distanceBetween(const std::map<std::vector<int>, ValDefaultNeg> & level, const std::vector<int> & start, const std::vector<int> & end)
+int distanceBetween(const Map<Vec<int>, ValDefaultNeg> & level, const Vec<int> & start, const Vec<int> & end)
 {
     auto itStart = level.find(start);
     auto itEnd = level.find(end);
@@ -146,40 +177,40 @@ int distanceBetween(const std::map<std::vector<int>, ValDefaultNeg> & level, con
     return constants::infinity;
 }
 
-void A_star(const std::vector<int> & start, const std::vector<int> & goal, Heuristic h) 
+void A_star(const Vec<int> & start, const Vec<int> & goal, Heuristic h) 
 {
     // The set of nodes already evaluated.
-    std::vector<std::vector<int>> closedSet;
+    Vec2d<int> closedSet;
 
     // The set of currently discovered nodes that are not evaluated yet.
     // Initially, only the start node is known.
-    std::vector<std::vector<int>> openSet;
+    Vec2d<int> openSet;
     openSet.push_back(start);
     
     // For each node, which node it can most efficiently be reached from.
     // If a node can be reached from many nodes, cameFrom will eventually contain the
     // most efficient previous step.
-    std::map<std::vector<int>, std::vector<int>> cameFrom;
+    Map<Vec<int>, Vec<int>> cameFrom;
     
     // For each node, the cost of getting from the start node to that node.
-    std::map<std::vector<int>, ValDefaultInf> gScore; // map with default value of Infinity
+    Map<Vec<int>, ValDefaultInf> gScore; // map with default value of Infinity
 
     // The cost of going from start to start is zero.
     gScore[start] = ValDefaultInf{0};
 
-    std::map<std::vector<int>, ValDefaultNeg> level;
+    Map<Vec<int>, ValDefaultNeg> level;
     level[start] = ValDefaultNeg{0}; // start is level 0
         
     // For each node, the total cost of getting from the start node to the goal
     // by passing by that node. That value is partly known, partly heuristic.
-    std::map<std::vector<int>, ValDefaultInf> fScore;
+    Map<Vec<int>, ValDefaultInf> fScore;
 
     // For the first node, that value is completely heuristic.
     fScore[start] = ValDefaultInf{heuristic(start, goal, h)};
 
     while (!openSet.empty()) 
     {
-        std::vector<int> current = lowestFScore(openSet, fScore); // the node in openSet having the lowest fScore[] value
+        Vec<int> current = lowestFScore(openSet, fScore); // the node in openSet having the lowest fScore[] value
         
         if (current == goal) 
         {
@@ -187,32 +218,32 @@ void A_star(const std::vector<int> & start, const std::vector<int> & goal, Heuri
             return;
         }
         
-         // remover nodo current do openSet
-        openSet.erase(std::remove(openSet.begin(), openSet.end(), current), openSet.end());
+        // remove current node from openSet
+        helpers::remove(openSet, current);
         
         closedSet.push_back(current);
 
-         // posicoes que ele pode mover 
-        std::vector<std::vector<int>> neighboors = calculateNeighboors(current);
+        // positions current can move
+        Vec2d<int> neighboors = calculateNeighboors(current);
 
         for (const auto & neighbor : neighboors) 
         {
             if (level.find(neighbor) == level.end()) // if neighbor's level was not setted yet
                 level[neighbor] = ValDefaultNeg{level[current].v + 1};
             
-             // procurar se está no closedSet
-            if (std::find(closedSet.begin(), closedSet.end(), neighbor) != closedSet.end())
+            // search if its in closeSet
+            if (helpers::find(closedSet, neighbor))
                 continue; // Ignore the neighbor which is already evaluated.
 
             int tentative_gScore = gScore[current].v + distanceBetween(level, current, neighbor);
 
-            // procurar se não está no openSet
-            if (std::find(openSet.begin(), openSet.end(), neighbor) == openSet.end())
+            // search if its not in openSet
+            if (!helpers::find(openSet, neighbor))
             {
-                openSet.push_back(neighbor); // descobriu um nodo novo e adicionou no openSet
+                openSet.push_back(neighbor); // find a new node and add to openSet
             }
             else if (tentative_gScore >= gScore[neighbor].v) 
-                continue; // Não é o melhor caminho
+                continue; // pass, not the best way
             
             cameFrom[neighbor] = current;
             gScore[neighbor] = ValDefaultInf{tentative_gScore};
@@ -220,12 +251,13 @@ void A_star(const std::vector<int> & start, const std::vector<int> & goal, Heuri
         }
     }
     
-    std::cout << "Não achou a solucao\n";
+    std::cout << "Nao achou a solucao\n";
 }
 
 int main()
 {
     std::ios::sync_with_stdio(false);
+
     A_star(
         {2, 8, 3, 1, 6, 4, 7, 0, 5}, // 6 passos
         //{8, 1, 3, 2, 4, 5, 0, 7, 6}, // 9 passos
